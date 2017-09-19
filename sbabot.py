b3903933beb2bfac47f41149e1c1487a1cb7a3c5
gartee <32103933+gartee@users.noreply.github.com>
2017-09-19 11:56:56 -0400

    sbabot.py
    Simple Slackbot designed to push students to google form

---
+++ sbabot.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, Response, jsonify
+
+app = Flask(__name__)
+link = '<Insert Text Here! Link Here >'
+
+
+@app.route('/lumbergh', methods=['POST'])
+def lumbergh():
+    text = request.form.get('text', '')
+    if ' ' in text.lower() and link not in text:
+        return jsonify(text=link)
+    return Response(), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
